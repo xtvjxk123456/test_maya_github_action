c2930eab665cd91fbc8af7627383b141feb7eb9c
zxq <xtvjxk123456maya@163.com>
2025-05-10 16:14:54 +0800

    build bat修改

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 set DEVKIT_LOCATION=D:\Code\mayaDevkit\2019.2\devkitBase
-cmake -A x64 -B D:\Code\test_maya-github_action\build -DCMAKE_BUILD_TYPE=Release
-cmake --build D:\Code\test_maya-github_action\build --config Release
-explorer D:\Code\test_maya-github_action\build\Release
+cmake -A x64 -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+explorer build
