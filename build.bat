b0068522e22665f64346b4ea31ab48c444a1bf74
zxq <xtvjxk123456maya@163.com>
2025-05-10 18:36:00 +0800

    modif build

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-set DEVKIT_LOCATION=D:\Code\mayaDevkit\2019.2\devkitBase
+set DEVKIT_LOCATION=D:\Code\mayaDevkit\2022.5\devkitBase
+rmdir /S /Q build
 cmake -A x64 -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
-explorer build
+copy build\Release %userprofile%\documents\maya\2022\plug-ins
+explorer %userprofile%\documents\maya\2022\plug-ins
